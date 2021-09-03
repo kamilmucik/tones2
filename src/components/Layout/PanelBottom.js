19d7fe24493653930c122787f48b6b73a19c04bf
kmucik <k.mucik@e-strix.pl>
2021-09-03 15:12:24 +0200

    try to add p5 lib

--- src/components/Layout/PanelBottom.js
+++ src/components/Layout/PanelBottom.js
@@ -4,8 +4,15 @@ import mainMicrophone from '../../assets/main_microphone.png';
 import mainTime from '../../assets/main_time.png';
 import Toggle from '../common/Toggle.component';
 import AppContext from '../../store/app-context';
+import p5 from "p5"
+// import "p5/lib/addons/p5.sound"
+// import p5 from 'https://cdn.skypack.dev/p5'
+// import 'https://cdn.skypack.dev/p5/lib/addons/p5.sound'
 
 const PanelBottom = props => {
+
+    let osc;
+
     const appCtx = useContext(AppContext);
     
     const checkMicrophoneIsCheckedHandler = () => {
@@ -16,6 +23,32 @@ const PanelBottom = props => {
         appCtx.setMicrophoneChecked(false);
     };
 
+    const playHandler = () => {
+        console.log("playHandler");
+
+        // // A triangle oscillator
+        // osc = new p5.TriOsc();
+        // // Start silent
+        // osc.start();
+        // osc.amp(0);
+
+        // playNote("A", 4000);
+    }
+
+    // A function to play a note
+function playNote(note, duration) {
+    // osc.freq(440);
+    // // Fade it in
+    // osc.fade(0.5,0.2);
+  
+    // // If we sest a duration, fade it out
+    // if (duration) {
+    //   setTimeout(function() {
+    //     osc.fade(0,0.2);
+    //   }, duration-50);
+    // }
+  }
+
     return <Fragment>
         <header className={classes.header}>
             <div>
@@ -24,6 +57,7 @@ const PanelBottom = props => {
                     <tr>
                         <td className={classes.buttonCenter}>
                             <a onClick={props.onShowModal}><img src={mainTime} /></a>
+                            <a onClick={playHandler}>Play</a>
                         </td>
                         <td className={classes.buttonCenter}>
                             <Toggle 
